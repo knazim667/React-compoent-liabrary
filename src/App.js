63c64d8bd91a14b3a8fbd574861d90c9dbd6ea11
knazim667 <nazamzahoor@yahoo.com>
2021-02-25 14:29:35 -0500

    add navabr with functionality still uncomplete add some packages and logo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
+import logo from "./assets/images/logo-2.png";
 
 function App() {
-  return <div className="App">hello world</div>;
+  return (
+    <div className="App">
+      <Navbar brand="Navbar" />
+      <Navbar brandImage={logo} />
+
+      <Navbar brand="Navbar" brandImage={logo} />
+    </div>
+  );
 }
 
 export default App;
