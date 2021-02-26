3a2849144ad6945d50a6ca342832c54c53c0f5fe
knazim667 <nazamzahoor@yahoo.com>
2021-02-25 21:52:16 -0500

    navbar component complete

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
+import React, { Component } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import logo from "./assets/images/logo-2.png";
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar brand="Navbar" />
-      <Navbar brandImage={logo} />
-
-      <Navbar brand="Navbar" brandImage={logo} />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+        { name: "Home", path: "/" },
+        { name: "About", path: "/about" },
+        { name: "Contact", path: "/contact" },
+      ],
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        {/* Navbar Start */}
+        <Navbar brand="Navbar" />
+        <Navbar brandImage={logo} />
+        <Navbar brand="Navbar" brandImage={logo} />
+        <Navbar brand="Navbar" data={this.state.data} />
+        <Navbar brand="Navbar" data={this.state.data} alignItem="m-auto" />
+        <Navbar brand="Navbar" data={this.state.data} alignItem="ml-auto" />
+        {/* Navbar End */}
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,116 +1,60 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+import Icon from "react-icons-kit";
 import { Link } from "react-router-dom";
 import "./navbar.css";
 import { navicon } from "react-icons-kit/fa/navicon";
 
-class Navbar extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-  render() {
-    const { brand, brandImage } = this.props;
+const Navbar = ({ brand, brandImage, data, alignItem = "mr-auto" }) => {
+  const [item, setItem] = useState(false);
 
-    return (
-      <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-3">
-        <div class="container">
-          {brand && brandImage ? (
-            <Link className="navbar-brand" to="/">
-              <img src={brandImage} alt="Brand Logo" className="brand-image" />
-              {brand}
-            </Link>
-          ) : brandImage ? (
-            <Link className="navbar-brand" to="/">
-              <img
-                src={brandImage}
-                alt="Brand Logo"
-                className="brand-image mr-0"
-              />
-            </Link>
-          ) : (
-            <Link className="navbar-brand" to="/">
-              {brand}
-            </Link>
-          )}
+  const classOne = item
+    ? "collapse navbar-collapse show"
+    : "collapse navbar-collapse";
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark mb-3">
+      <div className="container">
+        {brand && brandImage ? (
+          <Link className="navbar-brand" to="/">
+            <img src={brandImage} alt="Brand Logo" className="brand-image" />
+            {brand}
+          </Link>
+        ) : brandImage ? (
+          <Link className="navbar-brand" to="/">
+            <img
+              src={brandImage}
+              alt="Brand Logo"
+              className="brand-image mr-0"
+            />
+          </Link>
+        ) : (
+          <Link className="navbar-brand" to="/">
+            {brand}
+          </Link>
+        )}
 
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">
-                  Home
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  Link
-                </a>
-              </li>
-              <li class="nav-item dropdown">
-                <a
-                  class="nav-link dropdown-toggle"
-                  href="#"
-                  id="navbarDropdown"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Dropdown
-                </a>
-                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <li>
-                    <a class="dropdown-item" href="#">
-                      Action
-                    </a>
-                  </li>
-                  <li>
-                    <a class="dropdown-item" href="#">
-                      Another action
-                    </a>
-                  </li>
-                  <li>
-                    <hr class="dropdown-divider" />
-                  </li>
-                  <li>
-                    <a class="dropdown-item" href="#">
-                      Something else here
-                    </a>
-                  </li>
-                </ul>
-              </li>
-              <li class="nav-item">
-                <a
-                  class="nav-link disabled"
-                  href="#"
-                  tabindex="-1"
-                  aria-disabled="true"
-                >
-                  Disabled
-                </a>
-              </li>
-            </ul>
-            <form class="d-flex">
-              <input
-                class="form-control me-2"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-              />
-              <button class="btn btn-outline-success" type="submit">
-                Search
-              </button>
-            </form>
-          </div>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          onClick={() => setItem(!item)}
+        >
+          <Icon size="20" icon={navicon} />
+        </button>
+        <div className={classOne}>
+          <ul className={`navbar-nav mb-2 mb-lg-0 ${alignItem}`}>
+            {data &&
+              data.map((item, index) => (
+                <li key={index} className="nav-item">
+                  <Link className="nav-link" to={item.path}>
+                    {item.name}
+                  </Link>
+                </li>
+              ))}
+          </ul>
         </div>
-      </nav>
-    );
-  }
-}
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
